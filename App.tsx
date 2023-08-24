f4ccf455936b2ab5bfbf2d302ec6d1996b3b2f32
marcellasasso <marcellasasso@gmail.com>
2023-08-24 16:58:29 -0300

    até aula componente de input

--- App.tsx
+++ App.tsx
@@ -1,24 +1,30 @@
 /* eslint-disable camelcase */
-import { View, StatusBar } from 'react-native'
+import { StatusBar } from 'react-native'
 import { NativeBaseProvider } from 'native-base'
+
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto'
 
+import { THEME } from './src/theme'
+
+import { Loading } from '@components/Loading'
+import { SignIn } from '@screens/SignIn'
+
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
       <StatusBar
         barStyle={'light-content'}
-        backgroundColor="transparent"
+        backgroundColor={'transparent'}
         translucent
       />
 
-      {fontsLoaded ? <View /> : <View />}
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   )
 }
---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,40 @@
+import { VStack, Image, Text, Center, Heading } from 'native-base'
+
+import LogoSvg from '@assets/logo.svg'
+import backgrounImg from '@assets/background.png'
+import { Input } from '@components/Input'
+
+export function SignIn() {
+  return (
+    <VStack flex={1} bgColor={'gray.700'}>
+      <Image
+        source={backgrounImg}
+        alt="Pessoas treinando"
+        resizeMode={'contain'}
+        position={'absolute'}
+      />
+
+      <Center my={24}>
+        <LogoSvg />
+
+        <Text color={'gray.100'} fontSize={'sm'}>
+          Treine sua mente e o seu corpo
+        </Text>
+      </Center>
+
+      <Center>
+        <Heading
+          color={'gray.100'}
+          fontSize={'xl'}
+          mb={6}
+          fontFamily={'heading'}
+        >
+          Acesse sua conta
+        </Heading>
+
+        <Input placeholder="E-mail" />
+        <Input placeholder="Senha" />
+      </Center>
+    </VStack>
+  )
+}
