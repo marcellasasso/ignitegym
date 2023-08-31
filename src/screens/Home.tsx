db5d51a5787625844ae2e4bbaf7caf539caa6083
marcellasasso <marcellasasso@gmail.com>
2023-08-30 23:04:59 -0300

    até recap da home

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,14 +1,65 @@
-import { VStack } from 'native-base'
+import { useState } from 'react'
+import { FlatList, HStack, Heading, Text, VStack } from 'native-base'
 
 import { Group } from '@components/Group'
 import { HomeHeader } from '@components/HomeHeader'
+import { ExerciseCard } from '@components/ExerciseCard'
 
 export function Home() {
+  const [groups, setGroups] = useState(['Costas', 'Bíceps', 'Tríceps', 'Ombro'])
+  const [groupSelected, setGroupSelected] = useState(groups[0])
+
+  const [exercises, setExercises] = useState([
+    'Puxada frontal',
+    'Remada curvada',
+    'Remada unilateral',
+    'Levantamento terra',
+  ])
+
   return (
     <VStack flex={1}>
       <HomeHeader />
 
-      <Group name={'costas'} />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{
+          px: 8,
+        }}
+        my={10}
+        maxH={10}
+      />
+
+      <VStack flex={1} px={8}>
+        <HStack justifyContent={'space-between'} mb={5}>
+          <Heading color={'gray.200'} fontSize={'md'}>
+            Exercícios
+          </Heading>
+
+          <Text color={'gray.200'} fontSize={'sm'}>
+            {exercises.length}
+          </Text>
+        </HStack>
+
+        <FlatList
+          data={exercises}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => <ExerciseCard />}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{
+            paddingBottom: 20,
+          }}
+        />
+      </VStack>
     </VStack>
   )
 }
