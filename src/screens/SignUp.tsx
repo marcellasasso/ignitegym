63bff69700fea3abcd6dec114292de2d43b1e57e
marcellasasso <marcellasasso@gmail.com>
2023-09-07 11:15:20 -0300

    até aula entendendo o fluxo do back-end

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -45,18 +45,19 @@ export function SignUp() {
     navigation.goBack()
   }
 
-  function handleSignUp({
-    name,
-    email,
-    password,
-    passwordConfirm,
-  }: FormDataProps) {
-    console.log({
-      name,
-      email,
-      password,
-      passwordConfirm,
+  async function handleSignUp({ name, email, password }: FormDataProps) {
+    const response = await fetch('http://127.0.0.1:3333/users', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ name, email, password }),
     })
+
+    const data = await response.json()
+
+    console.log(data)
   }
 
   return (
